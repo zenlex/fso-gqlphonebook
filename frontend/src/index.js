62c912d5c4d434a96a3a07dd13b8a74cb36d6496
zenlex <erichrkeil@gmail.com>
2022-03-12 18:39:32 -0600

    WIP initial client hookup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000'
+  })
+})
+
+const query = gql`
+query {
+  allPersons {
+    name,
+    phone,
+    address {
+      street,
+      city
+    }
+    id
+  }
+}
+`
+//TODO: START HERE - need to spin up back end and front end and verify that test query logs. 
+client.query({ query })
+  .then((response) => {
+    console.log(response.data)
+  })
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
