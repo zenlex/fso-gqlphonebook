59b25ce4333eb2b9955c3671702869f8f3156e67
zenlex <erichrkeil@gmail.com>
2022-03-13 17:41:24 -0500

    parallel to 8-3 error handling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,21 +43,45 @@ const Persons = ({persons}) => {
           </button>
         </div>
       ))}
-      <PersonForm />
     </div>
   );
 }
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState('')
+
   const result = useQuery(ALL_PERSONS)
 
   if (result.loading) {
     return <div>loading...</div>
   }
 
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+    
+  }
   return(
+    <div>
+      <Notify errorMessage={errorMessage} />
     <Persons persons={result.data.allPersons} />
+    <PersonForm setError={notify} />
+
+
+    </div>
   )
 }
 
+const Notify = ({errorMessage}) =>{
+  if( !errorMessage ) {
+    return null
+  }
+  return(
+    <div style={{color: 'red'}}>
+      {errorMessage}
+    </div>
+  )
+}
 export default App;
--- frontend/src/components/PersonForm.js
+++ frontend/src/components/PersonForm.js
@@ -2,13 +2,18 @@ import { useState } from "react";
 import { useMutation } from '@apollo/client'
 import { CREATE_PERSON, ALL_PERSONS } from "../queries";
 
-const PersonForm = () => {
+const PersonForm = ({ setError }) => {
   const [name, setName] = useState('')
   const [phone, setPhone] = useState('')
   const [street, setStreet] = useState('')
   const [city, setCity] = useState('')
   
-  const [ createPerson ] = useMutation(CREATE_PERSON, { refetchQueries: [ {query: ALL_PERSONS }]})
+  const [ createPerson ] = useMutation(CREATE_PERSON, {
+    refetchQueries: [ {query: ALL_PERSONS }],
+    onError: (error) => {
+      setError(error.graphQLErrors[0].message)
+    }
+  })
 
   const submit = (event) => {
     event.preventDefault()
