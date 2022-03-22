ed8633179dd109d206f2232d19c572cde86c276b
zenlex <erichrkeil@gmail.com>
2022-03-22 09:52:06 -0500

    p8-6 refactor queries to use fragments

--- frontend/src/queries.js
+++ frontend/src/queries.js
@@ -2,11 +2,12 @@ import { gql } from '@apollo/client';
 
 export const LOGIN = gql`
   mutation login($username: String!, $password: String!) {
-    login(username: $username, password: $password){
+    login(username: $username, password: $password) {
       value
     }
-  }`
-    
+  }
+`;
+
 export const CREATE_PERSON = gql`
   mutation createPerson(
     $name: String!
@@ -26,27 +27,34 @@ export const CREATE_PERSON = gql`
   }
 `;
 
+const PERSON_DETAILS = gql`
+  fragment PersonDetails on Person {
+    id
+    name
+    phone
+    address {
+      street
+      city
+    }
+  }
+`;
+
 export const ALL_PERSONS = gql`
   query {
     allPersons {
-      name
-      phone
-      id
+      ...PersonDetails
     }
   }
+  ${PERSON_DETAILS}
 `;
 
 export const FIND_PERSON = gql`
   query findPersonByName($nameToSearch: String!) {
     findPerson(name: $nameToSearch) {
-      name
-      phone
-      address {
-        street
-        city
-      }
+      ...PersonDetails
     }
   }
+  ${PERSON_DETAILS}
 `;
 
 export const EDIT_NUMBER = gql`
